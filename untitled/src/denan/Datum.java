fd60f74f1bde4b41acfb1b7feaf82404b645e0cb
DenanP <denanpoturak@gmail.com>
2022-10-28 09:56:53 +0200

    V1

---
+++ untitled/src/denan/Datum.java
@@ -0,0 +1,10 @@
+package denan;
+
+public class Datum {
+  //Attributes
+  private int dan, mjesec, godina;
+  //Methods
+  public Datum(int dan, int mjesec, int godina) {this.dan = dan; this.mjesec = mjesec; this.godina = godina;}
+  public String Ispisi() {return dan + "." + mjesec + "." + godina + ".god";}
+}
+
